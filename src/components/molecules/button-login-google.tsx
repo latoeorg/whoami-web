8162f64f63d076003f141f3909425477c484a5a8
lazlanrafar <lazlanrafar@gmail.com>
2024-01-06 02:49:12 +0800

    feat(auth): login using google account

--- src/components/molecules/button-login-google.tsx
+++ src/components/molecules/button-login-google.tsx
@@ -1,9 +1,29 @@
+import supabase from "@/lib/supabase";
 import { Button } from "../atoms/button";
 import { Icons } from "../atoms/icons";
+import { siteConfig } from "@/constant";
+import { toast } from "sonner";
 
 export default function ButtonLoginGoogle() {
+  const signInWithGoogle = async () => {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: siteConfig.url,
+      },
+    });
+
+    if (error) {
+      toast.error(error.message);
+      return;
+    }
+
+    console.log(data);
+    toast.success("Login success");
+  };
+
   return (
-    <Button variant="outline">
+    <Button variant="outline" onClick={signInWithGoogle}>
       <Icons.gitHub className="mr-2 h-4 w-4" />
       Google
     </Button>
