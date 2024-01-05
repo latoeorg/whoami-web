ca22aabcbce12b770ce894ecce73fe4821c7f8af
lazlanrafar <lazlanrafar@gmail.com>
2024-01-06 00:01:12 +0800

    feat(header): slicing main nav

---
+++ src/components/app/site-header/index.tsx
@@ -0,0 +1,13 @@
+import { SiteHeaderMainNav } from "./main-nav";
+
+const SiteHeader = () => {
+  return (
+    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="container flex h-14 max-w-screen-2xl items-center">
+        <SiteHeaderMainNav />
+      </div>
+    </header>
+  );
+};
+
+export default SiteHeader;
---
+++ src/components/app/site-header/main-nav.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { siteConfig } from "@/constant";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export const SiteHeaderMainNav = () => {
+  const pathname = usePathname();
+
+  return (
+    <div className="mr-4 hidden md:flex">
+      <Link href="/" className="mr-6 flex items-center space-x-2">
+        <Logo />
+        <span className="hidden font-bold sm:inline-block">
+          {siteConfig.name}
+        </span>
+      </Link>
+
+      <nav className="flex items-center gap-6 text-sm">
+        <Link
+          href="/docs/components"
+          className={cn(
+            "transition-colors hover:text-foreground/80",
+            pathname?.startsWith("/docs/components")
+              ? "text-foreground"
+              : "text-foreground/60"
+          )}
+        >
+          Components
+        </Link>
+      </nav>
+    </div>
+  );
+};
+
+const Logo = () => {
+  return (
+    <svg
+      className="h-6 w-6"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 256 256"
+    >
+      <rect width="256" height="256" fill="none" />
+      <line
+        x1="208"
+        y1="128"
+        x2="128"
+        y2="208"
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="16"
+      />
+      <line
+        x1="192"
+        y1="40"
+        x2="40"
+        y2="192"
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="16"
+      />
+    </svg>
+  );
+};
