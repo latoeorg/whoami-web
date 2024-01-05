ee221584e97c7ed9120930394334e383cefd7b88
lazlanrafar <lazlanrafar@gmail.com>
2024-01-06 00:18:50 +0800

    feat(header): slicing mobile nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
 export default function Home() {
   return (
     <main>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione officiis dolor est iure aperiam.
-      Aspernatur, similique, culpa deleniti sapiente laboriosam aliquid maiores cupiditate ullam repellat quam
-      voluptas iusto excepturi velit?
+      {/* loop p 100 times */}
+
+      {Array.from({ length: 100 }, (_, i) => {
+        return (
+          <p key={i}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+            voluptatum. Quia, voluptatum. Quia, voluptatum. Quia, voluptatum.
+            Quia, voluptatum. Quia, voluptatum. Quia, voluptatum. Quia,
+            voluptatum. Quia, voluptatum. Quia, voluptatum. Quia, voluptatum.
+            Quia, voluptatum. Quia, voluptatum. Quia, voluptatum. Quia,
+            voluptatum. Quia, voluptatum. Quia, voluptatum. Quia, voluptatum.
+            Quia, voluptatum. Quia, voluptatum. Quia, voluptatum. Quia,
+            voluptatum. Quia, voluptatum. Quia, voluptatum. Quia, voluptatum.
+            Quia, voluptatum. Quia, voluptatum.
+          </p>
+        );
+      })}
     </main>
   );
 }
--- src/components/app/site-header/index.tsx
+++ src/components/app/site-header/index.tsx
@@ -1,10 +1,12 @@
 import { SiteHeaderMainNav } from "./main-nav";
+import { SiteHeaderMobileNav } from "./mobile-nav";
 
 const SiteHeader = () => {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center">
         <SiteHeaderMainNav />
+        <SiteHeaderMobileNav />
       </div>
     </header>
   );
